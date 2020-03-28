cc6a9ec1cde491522ab5df225d3bdde57f2895e4
Adam Cook <adam@cookuop.co.uk>
2020-03-28 23:05:55 +0000

    Update

--- Public/Get-DPContentDistributionStatus.ps1
+++ Public/Get-DPContentDistributionStatus.ps1
@@ -1,9 +1,21 @@
 function Get-DPContentDistributionStatus {
     <#
     .SYNOPSIS
-        Short description
-    .DESCRIPTION
-        Long description
+        Retrieve the content distribution status of all objects for a distribution point
+    .PARAMETER DistributionPoint
+        Name of distribution point (as it appears in ConfigMgr, usually FQDN) you want to query.
+    .PARAMETER SiteServer
+        Query SMS_DPContentInfo on this server.
+
+        It is not usually necessary to specify this parameter as importing the PSCMContentMgr module sets the $CMSiteServer variable which is the default value for this parameter.
+
+        Specify this to query an alternative server, or if the module import process was unable to auto-detect and set $CMSiteServer.
+    .PARAMETER SiteCode
+        Site code of which the server specified by -SiteServer belongs to.
+
+        It is not usually necessary to specify this parameter as importing the PSCMContentMgr module sets the $CMSiteCode variable which is the default value for this parameter.
+
+        Specify this to query an alternative site, or if the module import process was unable to auto-detect and set $CMSiteCode.
     .EXAMPLE
         PS C:\> <example usage>
         Explanation of what the example does
@@ -29,6 +41,8 @@ function Get-DPContentDistributionStatus {
             $PSCmdlet.ThrowTerminatingError($_)
         }
 
+        $OriginalLocation = (Get-Location).Path
+
         if($null -eq (Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue)) {
             New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $SiteServer -ErrorAction Stop | Out-Null
         }
@@ -37,15 +51,18 @@ function Get-DPContentDistributionStatus {
     }
     process {
         $Namespace = "ROOT/SMS/Site_{0}" -f $SiteCode
-        $Query = "SELECT * FROM SMS_PackageStatusDistPointsSummarizer WHERE NALPath like '%{0}%'" -f $DistributionPoint
+        $Query = "SELECT PackageID,PackageType,State,SourceVersion FROM SMS_PackageStatusDistPointsSummarizer WHERE ServerNALPath like '%{0}%'" -f $DistributionPoint
 
-        Get-CimInstance -ComputerName $SiteServer -Namespace $Namespace -Query $Query -ErrorAction "Stop" | Select-Object -Property @(
-            @{Label="ObjectID";Expression={$_.PackageID}}
-            @{Label="ObjectType";Expression={[SMS_PackageStatusDistPointsSummarizer_PackageType]$_.PackageType}}
-            @{Label="DistributionPoint";Expression={$DistributionPoint}}
-            SourceVersion
-            @{Label="State";Expression={[SMS_PackageStatusDistPointsSummarizer_State]$_.State}}
-        )
+        Get-CimInstance -ComputerName $SiteServer -Namespace $Namespace -Query $Query -ErrorAction "Stop" | ForEach-Object {
+            [PSCustomObject]@{
+                PSTypeName        = "PSCMContentMgmt"
+                ObjectID          = $_.PackageID
+                ObjectName        = [SMS_PackageStatusDistPointsSummarizer_PackageType]$_.PackageType
+                State             = [SMS_PackageStatusDistPointsSummarizer_State]$_.State
+                SourceVersion     = $_.SourceVersion
+                DistributionPoint = $DistributionPoint
+            }
+        }
     }
     end {
         Set-Location $OriginalLocation
