6e3c8b232af32da20503e4b4d05f4b25e54c7838
Adam Cook <adam@cookuop.co.uk>
2020-09-02 23:14:01 +0100

    Fixed build issue where it would fail due to ScriptsToProcess warning

--- invoke.build.ps1
+++ invoke.build.ps1
@@ -306,6 +306,6 @@ task CreateReleaseAsset {
 
 # Synopsis: Update documentation (-NewRelease or -UpdateDocs switch parameter)
 task UpdateDocs -If ($NewRelease.IsPresent -Or $UpdateDocs.IsPresent) {
-    Import-Module -Name $BuildRoot\build\$Script:ModuleName -Force -ErrorAction "Stop"
+    Import-Module -Name $BuildRoot\build\$Script:ModuleName -Force
     New-MarkdownHelp -Module $Script:ModuleName -OutputFolder $BuildRoot\docs -Force
 }
