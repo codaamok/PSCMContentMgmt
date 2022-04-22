658641c124ed24a77ed647fe4bdf0c29ceb78469
codaamok <me@cookadam.co.uk>
2022-04-22 09:32:12 +0100

    Updated custom.build.ps1

--- custom.build.ps1
+++ custom.build.ps1
@@ -24,18 +24,49 @@ param (
 )
 
 task PreBuild {
-    New-Item -Path $BuildRoot\build\$ModuleName\ConfigurationManager\ConfigurationManager.psm1 -ItemType File -Force
-    $Params = @(
-        Path = $BuildRoot\build\ConfigurationManager\ConfigurationManager.psd1
-        Guid = (New-Guid).Guid
-        RootModule = $BuildRoot\build\ConfigurationManager\ConfigurationManager.psm1
-    )
-    New-ModuleManifest @Params
-    Import-Module -Name $BuildRoot\build\ConfigurationManager\ConfigurationManager.psd1
+    if (-not (Get-Module 'ConfigurationManager' -ListAvailable)) {
+        Invoke-BuildClean -Path $BuildRoot\misc
+        New-Item -Path $BuildRoot\misc\ConfigurationManager\ConfigurationManager.psm1 -ItemType File -Force
+        $Params = @{
+            Path = '{0}\misc\ConfigurationManager\ConfigurationManager.psd1' -f $BuildRoot
+            Guid = (New-Guid).Guid
+            RootModule = '{0}\misc\ConfigurationManager\ConfigurationManager.psm1' -f $BuildRoot
+        }
+        New-ModuleManifest @Params
+
+        $SuccessfullyCopiedModule = $false
+        foreach ($ModulePath in $env:PSModulePath.Split([System.IO.Path]::PathSeparator)) {
+            try {
+                $Params = @{
+                    Path        = '{0}\misc\ConfigurationManager' -f $BuildRoot
+                    Destination = $ModulePath
+                    Recurse     = $true
+                    Force       = $true
+                    PassThru    = $true
+                    ErrorAction = 'Stop'
+                }
+                Copy-Item @Params
+                $SuccessfullyCopiedModule = $true
+            }
+            catch {
+                Write-Warning -Message ('Failed to copy dummy ConfigurationManager module to "{0}" ({1})' -f $ModuleName, $_.Exception.Message)
+            }
+        }
+
+        if (-not $SuccessfullyCopiedModule) {
+            Write-Error "Failed to copy dummy ConfigurationManager module to a PSModulePath folder" -ErrorAction "Stop"
+        }
+    }
 }
 
 task PostBuild {
+    Invoke-BuildClean -Path $BuildRoot\misc
 
+    Get-Module 'ConfigurationManager' -ListAvailable | ForEach-Object {
+        if ($_ -notmatch 'AdminConsole\\bin') {
+            Remove-Item -Path $_.ModuleBase -Recurse -Force -ErrorAction 'Stop'
+        }
+    }
 }
 
 task PreRelease {
